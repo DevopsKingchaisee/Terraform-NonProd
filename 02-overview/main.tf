595562bbab4a8cbbeee7ff8cdfe807481a2620a6
Shashank Tyagi <shashank.tyagi@rackspace.com>
2022-12-02 20:36:38 +0530

    terraform-data

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -10,8 +10,20 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
-  instance_type = "t2.micro"
+resource "aws_eip" "awselasticip" {
+  vpc = true
+  }
+resource "aws_eip_association" "awselasticip"{
+  instance_id   = aws_instance.AmazonEC2.id
+  allocation_id = aws_eip.awselasticip.id
 }
+
+resource "aws_instance" "AmazonEC2" {
+  ami           = "ami-026b57f3c383c2eec" # Amazon EC2 // us-east-1
+  instance_type = "t2.micro"
+  key_name = "orange"
+  security_groups = ["Linux-SG"]
+  tags = {
+    Name = "aws_ssm01"
+  }
+ }
